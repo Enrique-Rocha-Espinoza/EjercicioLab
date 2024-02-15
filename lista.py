35dbf5c190487cd696b1c0d903793e1e2c24f923
Enrique-Rocha-Espinoza <enriqresiglo21@gmail.com>
2024-02-15 12:43:42 -0600

    primer commit

---
+++ lista.py
@@ -0,0 +1,24 @@
+
+def captura():
+    a=str(input("inserte el nombre del trabajador: "))
+    b=int(input(f"cuanto gana por hora {a}"))
+    c=int(input(f"cuantas horas trabajo {a}"))
+
+    return a,b,c
+
+
+if __name__ == "__main__":
+    trabajadores=int(input("ucnatos trabajadores son:"))
+    i= 0
+    lista= []
+    while(i<trabajadores):
+        
+        a,b,c= captura()
+        print(f"{a} {b} {c}")
+        lista.append(a)
+        lista.append(b)
+        lista.append(c)
+        i+=1
+    
+    for x in range(0, len(lista), 2):
+        print(x)
---
+++ suma.py
@@ -0,0 +1,4 @@
+n= int(input("inserte el numero"))
+
+a= (n(n+1))/2
+print(f"\n la suma es: {a}")
